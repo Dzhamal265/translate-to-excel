8d3c66529cbee11657e6292b7a9879c4db283efd
Dzhamal Abdulbasirov <dzamal26abdulbasirov@gmail.com>
2020-06-29 17:14:26 +0300

    add keys

--- translate_to_csv.py
+++ translate_to_csv.py
@@ -7,6 +7,7 @@ import pandas as pd
 paths = tuple(Path(__file__).parent.joinpath('translate').iterdir())
 
 columns = tuple(map(lambda pth: str(pth).split('.')[0][-2:], paths))
+columns = (*columns, 'keys')
 
 
 def to_list(result, next_item):
@@ -19,14 +20,21 @@ def to_list(result, next_item):
 
 def main():
     df = pd.DataFrame()
+    keys = None
+    position = None
     for i, path in enumerate(paths):
         with open(str(path)) as f:
             translate = json.load(f)
         translate = dict(map(lambda key: (key, translate[key]), sorted(translate.keys())))
+        if keys is None:
+            keys = pd.Series(list(translate.keys()))
         translate = reduce(to_list, tuple(translate.values()))
         column_value = pd.Series(translate)
         df.insert(column=columns[i], loc=i, value=column_value)
-    df.to_excel('./export/translate-zuzan-website.xlsx')
+        position = i
+    position += 1
+    df.insert(column=columns[position], loc=position, value=keys)
+    df.to_csv('./export/translate-zuzan-website.csv')
 
 
 if __name__ == '__main__':
